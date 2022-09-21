3ebb89bc71fb2e52242773e711ba4bed640e5478
Paulo Henrique <ph46163835@gmail.com>
2022-09-21 20:38:07 -0300

    conectando a Mongo Atlas

--- app.js
+++ app.js
@@ -6,4 +6,16 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
-app.listen(3000)
+app.get('/', (request, response) => {
+response.status(200).json({msg: "Acesso a Api"})
+})
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.qqskcdq.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000)
+    console.log("conectou com banco!")
+}).catch((err) => console.log(err))
+
